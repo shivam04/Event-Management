27ae582363be2e0e800595c2d787ed844f98350f
shivam04 <sinhahsivam04@gmail.com>
2017-01-20 16:11:50 +0530

    search model

--- emt_project/emt_project/settings.py
+++ emt_project/emt_project/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'emt_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'emt',
+        'USER': 'root',
+        'PASSWORD': '123',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
--- emt_project/search/admin.py
+++ emt_project/search/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import (
+	City,
+	Venues,
+	Locality,
+	Address,
+	)
+class CityAdmin(admin.ModelAdmin):
+	list_display = ["id","city_name"]
+	class Meta:
+		model = City
 
-# Register your models here.
+class LocalityAdmin(admin.ModelAdmin):
+	list_display = ["id","locality_name"]
+	class Meta:
+		model = City
+
+class VenuesAdmin(admin.ModelAdmin):
+	list_display = ["id","venue_name","venue_locality","venue_city"]
+	class Meta:
+		model = City
+
+class AddressAdmin(admin.ModelAdmin):
+	list_display = ["id","venue","address_line","zip_code"]
+	class Meta:
+		model = City
+
+
+admin.site.register(City,CityAdmin)
+admin.site.register(Locality,LocalityAdmin)
+admin.site.register(Venues,VenuesAdmin)
+admin.site.register(Address,AddressAdmin)
